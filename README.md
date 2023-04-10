7697d3bdbe1ef74d39df8004e95ac6ec0dd3fc18
themayank-goyal <77292986+themayank-goyal@users.noreply.github.com>
2023-04-10 12:53:54 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vibrate
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-rpxn2b)
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div className="app">
+      <div className="app__header">
+        <img className="app__headerImage" src="https://th.bing.com/th/id/OIP.fXbsIW9i7E60H1DSeb-qsQAAAA?w=163&h=61&c=7&r=0&o=5&dpr=1.3&pid=1.7" alt="Instagram" />
+      </div>
+    </div>
+  );
+}
